1b768a06061ef4daaba63e05242637e311a66056
유종화 <sola201@naver.com>
2025-08-03 21:52:59 +0900

    feat: implement UserService OCP for learning

---
+++ src/Ocp/DiscountServiceOCP.java
@@ -0,0 +1,50 @@
+package Ocp;
+
+import java.util.List;
+
+interface DiscountPolicy  {
+    boolean supports(String memberType);
+    int getDiscountRate();
+}
+
+class RegularDiscountPolicy implements DiscountPolicy {
+
+    @Override
+    public boolean supports(String memberType) {
+        return "Regular".equals(memberType);
+    }
+
+    @Override
+    public int getDiscountRate() {
+        return 5;
+    }
+}
+
+class VipDiscountPolicy implements DiscountPolicy {
+
+    @Override
+    public boolean supports(String memberType) {
+        return "VIP".equals(memberType);
+    }
+
+    @Override
+    public int getDiscountRate() {
+        return 20;
+    }
+}
+
+public class DiscountServiceOCP {
+    private final List<DiscountPolicy> policies;
+
+    public DiscountServiceOCP(List<DiscountPolicy> policies) {
+        this.policies = policies;
+    }
+
+    public int getDiscountRate(String memberType) {
+        return policies.stream()
+                .filter(p -> p.supports(memberType))
+                .findFirst()
+                .map(DiscountPolicy::getDiscountRate)
+                .orElse(0);
+    }
+}
