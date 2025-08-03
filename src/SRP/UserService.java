56e84a657f9f0b9ff1c55a90cecbca4e213fdcdc
유종화 <sola201@naver.com>
2025-08-03 21:23:39 +0900

    feat: implement UserService violating SRP for learning

---
+++ src/SRP/UserService.java
@@ -0,0 +1,24 @@
+package SRP;
+
+public class UserService {
+
+    public void registerUser(String userName, String password) {
+
+        System.out.println("Register user: " + userName);
+
+        System.out.println("save to log:" + userName);
+
+        System.out.println("Send welcome email to " +userName);
+
+        /*
+        *   what's wrong?
+        *   UserService class have 3 principal
+        *   1. Register user
+        *   2. save log
+        *   3. send email
+        *
+        *   if someone chang then maybe get effict antoher things too
+        * */
+
+    }
+}
